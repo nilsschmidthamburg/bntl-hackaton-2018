737e6c12330f9c2de7ec72e30c0eda76bd3a795c
Nils Schmidt <nils.schmidt@ottogroup.com>
2018-04-07 14:10:10 +0200

    Add a dynamic map with simple button

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = new Vue({
-  el: '#header',
+  el: '#main',
   data: {
-    message: 'bntl - digital - kreativ'
+    message: 'bntl - digital - kreativ',
+    src: 'https://overpass-turbo.eu'
+  },
+  methods: {
+    changeSrc: function () {
+      this.src = "https://overpass-turbo.eu/map.html?Q=area%5Bname%3D%22Baunatal%22%5D%3B%0A(way(area)%5Bhighway%5D%5Bmaxspeed%3D30%5D%3B%3E%3B)%3B%0Aout%20meta%3B%0A"
+    }
   }
 })
